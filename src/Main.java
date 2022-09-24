5af1f10512d298cdbbf551e2674c38c0e55fb67d
zemscky <aleksei.zemscky@yandex.ru>
2022-09-24 15:15:31 +0300

    Домашняя раота 3 , задание 2

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,19 @@ public class Main {
         int cometYears = 0;
         int lastYears = 1822; //  год за 200 лет до текущего
         int futureYears = 2122; // 100 лет после, в качестве завершения периода расчета
-        while (cometYears<2122) {
-            if (cometYears>=1822) {
-                System.out.println( cometYears + " - год комета пролетала рядом с Землей");
+        while (cometYears < 2122) {
+            if (cometYears >= 1822) {
+                System.out.println(cometYears + " - год комета пролетала рядом с Землей");
             }
             cometYears += 79;
+        }
+        // Задание 2
+        System.out.println("Задание 2");
+        int i = 2;
+        for (int j = 1; j < 11; j++) {
+            int sum = 0;
+            sum = i * j;
+            System.out.println(i + "*" + j + "=" + sum);
         }
 
     }
